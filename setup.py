39b91eff66255f3c82b26848f5a354e1c07f6528
Trond Hindenes <trond@hindenes.com>
2020-03-13 09:05:42 +0100

    Added needed package data

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setup(
     zip_safe=False,
     entry_points={
         'console_scripts': ['awshelper=aws_helper.cmd_line:main'],
-    }
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',  # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
